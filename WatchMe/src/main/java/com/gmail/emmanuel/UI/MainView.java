bad2f171e43cafe54b6a3469789b5ceaccdc6ab5
Heming Wu <hwu0114@berkeley.edu>
2020-05-30 01:57:45 -0700

    after log in

--- WatchMe/src/main/java/com/gmail/emmanuel/UI/MainView.java
+++ WatchMe/src/main/java/com/gmail/emmanuel/UI/MainView.java
@@ -1,6 +1,7 @@
 package com.gmail.emmanuel.UI;
 
 import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dependency.CssImport;
@@ -8,59 +9,62 @@ import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Header;
 import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.HighlightConditions;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouterLink;
 import com.vaadin.flow.server.PWA;
 import org.springframework.beans.factory.annotation.Autowired;
 
-/**
- * A sample Vaadin view class.
- * <p>
- * To implement a Vaadin view just extend any Vaadin component and
- * use @Route annotation to announce it in a URL as a Spring managed
- * bean.
- * Use the @PWA annotation make the application installable on phones,
- * tablets and some desktop browsers.
- * <p>
- * A new instance of this class is created for every new user and every
- * browser tab/window.
- */
+import javax.swing.text.html.ListView;
+
 @Route
 @PWA(name = "Vaadin Application",
         shortName = "Vaadin App",
         description = "This is an example Vaadin application.",
         enableInstallPrompt = false)
-@CssImport("./styles/shared-styles.css")
-//@CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
 @CssImport("frontend://styles/styles.css")
 public class MainView extends VerticalLayout {
 
     public MainView() {
-        addClassName("main-view");
+//        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
 
+        addClassName("main-view");
         H1 header = new H1("WatchMe");
         header.getElement().getThemeList().add("dark");
-
         add(header);
-        // Use TextField for standard text input
-        TextField textField = new TextField("Your name");
 
-        // Button click listeners can be defined as lambda expressions
-        Button button = new Button("Say hello");
+        askUserInfo();
 
-        // Theme variants give you predefined extra styles for components.
-        // Example: Primary button has a more prominent look.
-        button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
 
-        // You can specify keyboard shortcuts for buttons.
-        // Example: Pressing enter in this view clicks the Button.
-        button.addClickShortcut(Key.ENTER);
 
-        // Use custom CSS classes to apply styling. This is defined in shared-styles.css.
-//        addClassName("centered-content");
 
-        add(textField, button);
     }
+
+    private void askUserInfo() {
+        VerticalLayout startView = new VerticalLayout();
+        TextField nameField = new TextField("Your name");
+        TextField roomNumFiled = new TextField("Room Number");
+        Button startButton = new Button("Enter the Room");
+        startButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        startButton.addClickShortcut(Key.ENTER);
+        startView.add(nameField, roomNumFiled, startButton);
+        startView.setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        add(startView);
+
+        startButton.addClickListener(buttonClickEvent -> {
+            user = nameField.getValue();
+            roomNum = roomNumFiled.getValue();
+            removeAll();
+            MainLayout a = new MainLayout();
+            add(a);
+        });
+    }
+
+    static String user;
+    static String roomNum;
 
 }
